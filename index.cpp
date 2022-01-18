9500ed679721334715543e9c1af86f99f5e21b7a
lisabroadhead <37422388+lisabroadhead@users.noreply.github.com>
2022-01-18 13:16:16 -0600

    Update index.cpp
    updates to code

--- index.cpp
+++ index.cpp
@@ -6,13 +6,14 @@ using namespace std;
 
 class Pizza{
 public:
-	Pizza(string meat_i,string toppings_i,string sauce_i,string cheese_i,int size_i,string crust_i){
+	Pizza(string meat_i,string toppings_i,string sauce_i,string cheese_i,string size_i,string crust_i,string customer_name_i){
 		meat = meat_i;
 		toppings = toppings_i;
 		sauce = sauce_i;
 		cheese = cheese_i;
 		size = size_i;
 		crust = crust_i;
+		customer_name = customer_name_i;
 	}
 	string get_meat(){
 		return meat;
@@ -33,32 +34,67 @@ public:
 		return crust;
 	}
 
+	string get_customer_name(){
+		return customer_name;
+	}
+
+	cout << "ORDER FOR " << customer_name << endl;
+
+	cout << "\n Alright I have an order for 1 " << size << " " << crust << " stuffed pizza with" << sauce
+ << " sauce. Topped with " << meat << ", " << toppings << ", and " << cheese << " cheese!" << endl;
+
 private:
 	string meat;
 	string toppings;
 	string sauce;
 	string cheese;
-	int size;
+	string size;
 	string crust;
+	string customer_name;
 };
 
 int main(){
-	int var_meat[3];
+	string var_meat;
 	string var_toppings;
 	string var_sauce;
 	string var_cheese;
-	int var_size = 0;
+	string var_size;
 	string var_crust;
+	string var_answer = "yes";
+	string var_customer_name;
 
 	string moreMeat;
 
-	cout << "Welcome to Mario's Pizza! what can I get for you today? To start with whay kind of meat: " << endl;
-	cin >>  var_meat[0]; 
-	cout << "Would you like another meat? y / n" << endl;
-	gets(moreMeat); 
-	getline(cin,moreMeat);
+	cout << "Welcome to Mario's Pizza! Where you can build your own pizza! We're runniing a special on our larges for $9.99, would you like that?" << endl;
+	cin >> var_answer;
 
-	cout << moreMeat << endl;
+	if(var_answer == "no"){
+		cout << "What size wold you like?" << endl;
+		cin >> var_size;
+	}else{
+		var_size = "large";
+	}
+
+	cout << "What kind of meat would you like on that?" << endl;
+	cin >>  var_meat; 
+	cout << "Ok " << var_meat << "! Now what kind of sauce?" << endl;
+	cin >>  var_sauce;
+	cout << "Mmmm " << var_sauce << " is my favorite. Cheese?" << endl;
+	cin >> var_cheese; 
+	cout << var_cheese << " is not what I'd have chosen, but thats ok! Toppings, list 3 comma seperated" << endl;
+	cin >> var_toppings;
+	cout << "Now what kind of crust would you like" << endl;
+	cin >> var_crust;
+	cout << var_crust << " now we're talking." << endl;
+
+	cout << "And you name on the order" << endl;
+	cin >> var_customer_name;
+
+	cout << "Thank you " << var_customer_name << " your order should be ready in 10 minutes!" << endl;	
+
+
+
+	Pizza "pizza_"+var_customer_name;
 
 
 
