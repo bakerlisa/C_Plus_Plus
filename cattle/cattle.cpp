650f244e39af16fa66030edcaf2aca403332e511
Lisa Broadhead <37422388+lisabroadhead@users.noreply.github.com>
2022-01-20 12:43:35 -0600

    Update cattle.cpp
    Start of my cow code - I'm now goin to try and use git on my desktop to access, update and commit. Instead of updating the code here everytime I make an update

--- cattle/cattle.cpp
+++ cattle/cattle.cpp
@@ -1,1 +1,53 @@
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum cow_purpose{dairy,meat,hide,pet};
+
+class cow{
+	public:
+	cow(string name_i,int age_i,unsigned char purpose_i){
+		name = name_i;
+		age = age_i;
+		purpose = purpose_i;
+	}
+
+	string get_name(){
+		return name;
+	}
+	int get_age(){
+		return age;
+	}
+	unsigned char get_purpose(){
+		return purpose;
+	}
+
+	void set_age(int new_age){
+		age = new_age;
+	}
+	void set_name(string new_name){
+		name = new_name;
+	}
+	void set_purpose(unsigned char new_purpose){
+		purpose = new_purpose;
+	}
+	private:
+	string name;
+	int age;
+	unsigned char purpose;
+};
+
+
+int main(){
+   vector<cow> cattle;
+
+    cattle.push_back(cow("Betsy",19,pet));
+
+    cout << "The sizer of the vector Cattle is: " <<  cattle.size() << endl;
+    cout << "The first cow is named: " << cattle[0].get_name() << endl;
+
+	return 0;
+}
