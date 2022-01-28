e3b9cc2ae229b5fd7f6d973f130edc4ee62c171a
Lisa <lb.broadhead@gmail.com>
2022-01-28 10:05:02 -0600

    adding algo files and folders

---
+++ algos/day5/algoThree.cpp
@@ -0,0 +1,34 @@
+// JS CODE: 
+// var arr = ["a", "b", "c", "d", "e"];
+
+// function reverseArr(arr){
+//     let newArr = [];
+//     for(var i = (arr.length - 1); i >= 0;i--){
+//         newArr.push(arr[i]);
+//     }
+//     return newArr; 
+// }
+// console.log(reverseArr(arr));
+
+
+// function reverseTwo(){
+//     let newArr = [];
+//     let element = arr.length - 1; 
+ 
+//     while(element >= 0){
+//         newArr.push(arr[element]);
+//     }
+//     return newArr; 
+// }
+// console.log(reverseArr(arr));
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
