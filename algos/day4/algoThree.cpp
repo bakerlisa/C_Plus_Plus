2b5295fb492b0dacf6d9c04c085d7e756b74434d
Lisa <lb.broadhead@gmail.com>
2022-01-29 10:05:31 -0600

    algos for day 4 ex 3

---
+++ algos/day4/algoThree.cpp
@@ -0,0 +1,27 @@
+// JS CODE:
+
+// var start = 0;
+// var end = 12;
+    
+// while(start < end) {
+//     console.log("start: " + start + ", end: " + end);
+//     start += 2;
+//     end -= 2;
+// }
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int start = 0;
+    int end = 12;
+
+    while(start < end){
+        cout << "Start: " << start << " End: " << end << endl;
+        start += 2;
+        end -= 2;
+    }
+    return 0;
+}
--- algos/day4/tempCodeRunnerFile.cpp
+++ algos/day4/tempCodeRunnerFile.cpp
@@ -1,1 +1,8 @@
-fruit2 = fruit1;
+var start = 0;
+// var end = 12;
+    
+// while(start < end) {
+//     console.log("start: " + start + ", end: " + end);
+//     start += 2;
+//     end -= 2;
+// }
