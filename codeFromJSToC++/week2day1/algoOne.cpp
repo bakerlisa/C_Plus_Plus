667f31e70350530e0db0ceffd8f3dcc861de814f
Lisa <lb.broadhead@gmail.com>
2022-01-31 10:56:50 -0600

    todays alogs

---
+++ codeFromJSToC++/week2day1/algoOne.cpp
@@ -0,0 +1,22 @@
+// JS CODE
+
+// var floor = Math.floor(1.8);
+// var ceiling = Math.ceil( Math.PI );
+// var random = Math.random();
+    
+// console.log(floor);
+// console.log(ceiling);
+// console.log(random);
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    double floor = floor(double 1.8);
+    return 0;
+}
