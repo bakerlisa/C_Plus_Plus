cbcca422855fc12f9e53b1b0807215f4d55a20d8
Lisa <lb.broadhead@gmail.com>
2022-01-31 21:45:12 -0600

    js to c++ algos

---
+++ codeFromJSToC++/week2day1/algosThree.cpp
@@ -0,0 +1,68 @@
+// JS CODE
+// Creat an 8 ball
+
+// var lifesAnswers = [
+//     "It is certain.",
+//     "It is decidedly so.",
+//     "Without a doubt.",
+//     "Yes – definitely.",
+//     "You may rely on it.",
+//     "As I see it, yes.",
+//     "Most likely.",
+//     "Outlook good.",
+//     "Yes.",
+//     "Signs point to yes.",
+//     "Reply hazy, try again.",
+//     "Ask again later.",
+//     "Better not tell you now.",
+//     "Cannot predict now.",
+//     "Concentrate and ask again.",
+//     "Don't count on it.",
+//     "My reply is no.",
+//     "My sources say no.",
+//     "Outlook not so good.",
+//     "Very doubtful."
+// ];
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ranNum(){
+    srand(time(0));
+    int ranNum = rand() % 20;
+    return ranNum;
+}
+
+int main(){
+    string lifesAnswers[20] = {
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes – definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful."
+    };
+
+    int answer = ranNum();
+    cout << lifesAnswers[answer] << endl;
+
+    return 0;
+}
+
+//Over all good practice on functions and radom numbers
--- codeFromJSToC++/week2day1/algosTwo.cpp
+++ codeFromJSToC++/week2day1/algosTwo.cpp
@@ -1,12 +1,13 @@
 // JS CODE
-function d6() {
-    var roll = Math.random();
-    // your code here
-    return roll;
-}
-    
-var playerRoll = d6();
-console.log("The player rolled: " + playerRoll);
+
+// function d6() {
+//     var roll = Math.random();
+//     // your code here
+//     return roll;
+// }
+
+// var playerRoll = d6();
+// console.log("The player rolled: " + playerRoll);
 
 #include <iostream>
 #include <vector>
@@ -14,7 +15,17 @@ console.log("The player rolled: " + playerRoll);
 #include <vector>
 
 using namespace std;
+ int d6(){
+        srand(time(0));
+        int roll = rand() % (6 - 2) + 3; 
+        return roll;
+    }
 
 int main(){
+    int playerRoll = d6();
+    cout << "The player rolled: " << playerRoll << endl;
+    
     return 0;
 }
+
+// Even thoug the formating is the same, functins get me. Remembering to seed the rand, and the max and min width. I was also trying to see if I could get the numbers between two integers
